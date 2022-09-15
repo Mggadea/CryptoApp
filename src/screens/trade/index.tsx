61cddb87f71d6b035352f6b08bdd9569ad985cd8
Mggadea <matiasgaraygadea@outlook.com>
2022-09-15 19:36:45 -0300

    precios

--- src/screens/trade/index.tsx
+++ src/screens/trade/index.tsx
@@ -5,11 +5,14 @@ import {GobalStyles} from '../../assets/styles/globalStyles';
 import {Button, TokenPrice} from '@components';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import PricesContext from '../../context/pricesContext';
-import useTokens from '../../hooks/useTokens';
+import {BalanceContext} from '../../context';
 
 const Trade = () => {
   const {handleRefresh, priceBtc, priceEth, priceUsdc} =
     useContext(PricesContext);
+
+  const {orderBook} = useContext(BalanceContext);
+
   const navigation = useNavigation();
   const {params} = useRoute();
 
@@ -18,35 +21,43 @@ const Trade = () => {
 
   const {token} = params;
 
-  const wea = useTokens(token);
-
   const prices = () => {
     switch (selectedToken) {
       case 'BTC':
         setPrice(priceBtc);
         break;
-        case 'ETH':
+      case 'ETH':
         setPrice(priceEth);
         break;
-        case 'USDC':
+      case 'USDC':
         setPrice(priceUsdc);
         break;
-    }
-    if (selectedToken === 'BTC') {
-      setPrice(priceBtc);
-    }
-    if (selectedToken === 'ETH') {
-      setPrice(priceEth);
-    }
-    if (selectedToken === 'USDC') {
-      setPrice(priceUsdc);
     }
   };
 
   useEffect(() => {
+    switch (token) {
+      case 'BTC':
+        setPrice(priceBtc);
+        break;
+      case 'ETH':
+        setPrice(priceEth);
+        break;
+      case 'USDC':
+        setPrice(priceUsdc);
+        break;
+    }
+  }, [
+    priceBtc,
+    priceEth,
+    priceUsdc,
+    token,
+  ]);
+
+  useEffect(() => {
     setSelectedToken(token);
     prices();
-    console.log('sda', wea);
+    console.log('trade', orderBook);
   }, []);
 
   const handleBuy = () => {
@@ -72,56 +83,7 @@ const Trade = () => {
           <Button text={'Buy'} color="#00CC66" onPress={handleBuy} />
         </View>
       </View>
-      <Text style={GobalStyles.title}>OrderBook</Text>
-
-      <View
-        style={{
-          borderRadius: 10,
-          backgroundColor: '#fff',
-          shadowColor: '#000',
-          shadowOffset: {
-            width: 0,
-            height: 2,
-          },
-          shadowOpacity: 0.25,
-          shadowRadius: 3.84,
-          elevation: 5,
-          paddingVertical: 20,
-          marginBottom: 10,
-        }}>
-        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-          <View style={[styles.buttonContainer, {borderRightWidth: 1}]}>
-            <Text
-              style={{fontSize: 16, textAlign: 'center', fontWeight: 'bold'}}>
-              Sell orders
-            </Text>
-            <View
-              style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-              <Text>Amunt</Text>
-              <Text>Price</Text>
-            </View>
-            <View
-              style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-              <Text>0.12312312</Text>
-              <Text>20000</Text>
-            </View>
-          </View>
-          <View style={styles.buttonContainer}>
-            <Text
-              style={{fontSize: 16, textAlign: 'center', fontWeight: 'bold'}}>
-              Buy orders
-            </Text>
-            <View
-              style={{
-                flexDirection: 'row-reverse',
-                justifyContent: 'space-around',
-              }}>
-              <Text>Amunt</Text>
-              <Text>Price</Text>
-            </View>
-          </View>
-        </View>
-      </View>
+  
     </View>
   );
 };
