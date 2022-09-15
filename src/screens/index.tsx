74cab0b0cff28897c0b8e2e379312da9bb6adc99
Mggadea <matiasgaraygadea@outlook.com>
2022-09-14 22:56:49 -0300

    add limit screen

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -2,3 +2,5 @@ export {default as HomeScreen} from './home';
 export {default as Convert} from './convert';
 export {default as Trade} from './trade';
 export {default as Wallet} from './wallet';
+export {default as Limit} from './limit';
+
