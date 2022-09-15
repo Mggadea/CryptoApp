68393f7671f8240d557d456101cc647edfd8392a
Mggadea <matiasgaraygadea@outlook.com>
2022-09-14 22:57:12 -0300

    add alias

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export {default as IconSymbol} from './iconSymbol';
+export {default as MarketItem} from './marketItem';
+export {default as MarketOrLimit} from './marketOrLimit';
+export {default as TokenPrice} from './tokenPrice';
+export {default as Button} from './button'
