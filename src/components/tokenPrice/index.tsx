7055d43d14fba34469c0b8874c9c50f44c4f6405
Mggadea <matiasgaraygadea@outlook.com>
2022-09-14 22:58:31 -0300

    add token price component

---
+++ src/components/tokenPrice/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native'
+import IconSymbol from '../iconSymbol'
+import React from 'react'
+import {GobalStyles} from '../../assets/styles/globalStyles';
+
+
+const TokenPrice = ({token, price}) => {
+
+  return (
+    <View style={[styles.row, styles.container]}>
+    <View style={styles.row}>
+        <IconSymbol symbol={token} />
+        <Text style={[GobalStyles.title, {marginLeft:10}]}>{token}</Text>
+      </View>
+      <Text style={GobalStyles.title}>$ {Number(price).toFixed(2)}</Text>
+    </View>
+
+  )
+}
+
+export default TokenPrice
+
+const styles = StyleSheet.create({
+  container:{
+    width:'100%',
+  },
+row:{
+    flexDirection:'row', 
+    justifyContent:'space-between'
+}
+})
