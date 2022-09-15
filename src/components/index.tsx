9da65712619ef5014c7637ff8ebd38290c48ce24
Mggadea <matiasgaraygadea@outlook.com>
2022-09-15 17:23:03 -0300

    add components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,6 @@ export {default as IconSymbol} from './iconSymbol';
 export {default as MarketItem} from './marketItem';
 export {default as MarketOrLimit} from './marketOrLimit';
 export {default as TokenPrice} from './tokenPrice';
-export {default as Button} from './button'
+export {default as Button} from './button';
+export {default as OrderBook} from './orderBook';
+
